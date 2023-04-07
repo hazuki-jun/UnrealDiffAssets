35ad5c035f4c0066b53416b61c2260a6f134e403
hazuki <panjiahui1997@gmail.com>
2023-04-07 17:53:59 +0800

    # Toolbar

---
+++ Source/UnrealDiffAssetsEditor/Private/UnrealDiffAssetSettings.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UnrealDiffAssetSettings.h"
+
+#define LOCTEXT_NAMESPACE "UUnrealDiffAssetSettings"
+
+UUnrealDiffAssetSettings::UUnrealDiffAssetSettings()
+{
+	CategoryName = TEXT("Plugins");
+	SectionName  = TEXT("UnrealDiffAsset");
+}
+
+#if WITH_EDITOR
+FText UUnrealDiffAssetSettings::GetSectionText() const
+{
+	return LOCTEXT("SettingsDisplayName", "UnrealDiffAsset");
+}
+
+void UUnrealDiffAssetSettings::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	const FString FileName = FPaths::ProjectConfigDir() / FString(TEXT("DefaultUnrealDiffAssetSettings.ini"));
+	GConfig->Flush(true, FileName);
+}
+#endif
+
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/UnrealDiffAssetsEditor/Public/UnrealDiffAssetSettings.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "UnrealDiffAssetSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(config = UnrealDiffAssetSettings, defaultconfig)
+class UNREALDIFFASSETSEDITOR_API UUnrealDiffAssetSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+public:
+	UUnrealDiffAssetSettings();
+
+#if WITH_EDITOR
+	virtual FText GetSectionText() const override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	//~ Begin DataTableDiff Config
+	UPROPERTY(EditAnywhere, Config, Category = DataTableDiff, DisplayName = "ShowOnlyNormal")
+	bool DataTableVisualDiffShowOnlyNormal = true;
+
+	UPROPERTY(EditAnywhere, Config, Category = DataTableDiff, DisplayName = "ShowOnlyModify")
+	bool DataTableVisualDiffShowOnlyModify = true;
+
+	UPROPERTY(EditAnywhere, Config, Category = DataTableDiff, DisplayName = "ShowOnlyAdded")
+	bool DataTableVisualDiffShowOnlyAdded = true;
+
+	// UPROPERTY(EditAnywhere, Config, Category = DataTableDiff, DisplayName = "ShowOnlyRemoved")
+	// bool DataTableVisualDiffShowOnlyRemoved = true;
+	//~ End DataTableDiff Config
+};
